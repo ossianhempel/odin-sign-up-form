a58bade37fc026736f983dffb88a9f372f273272
ossianhempel <hemposse@hotmail.com>
2022-09-27 20:35:00 +0200

    Add js to validate that password and confirm password match

--- myScript.js
+++ myScript.js
@@ -1,2 +1,37 @@
-console.log('Hello, World');
+function samePassword() {
+    pw1 = document.querySelector("#password");
+    pw2 = document.querySelector("#confirm-password");
+    
+    if (pw1.value != pw2.value) {
+        pw1.classList.add("password-error");
+        pw2.classList.add("password-error");
+        formContainer.appendChild(pwErrorMessage);
+
+    } else {
+        pw1.classList.remove("password-error");
+        pw2.classList.remove("password-error");
+        formContainer.removeChild(pwErrorMessage);
+
+    }
+    
+};
+
+
+
+const createAcccountBtn = document.querySelector('button');
+const pwErrorMessage = document.createElement('p');
+pwErrorMessage.textContent = "*Passwords do not match";
+pwErrorMessage.classList.add("password-error");
+const formContainer = document.querySelector('.right-mid-container');
+
+
+
+
+
+
+
+
+
+
+createAcccountBtn.addEventListener('click', () => samePassword());
 
